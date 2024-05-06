23533230beafe52a5b368e7138d744f5eb67e994
Osiris8 <miganosiris8@gmaaail.com>
2024-05-06 13:19:28 +0200

    add job page

---
+++ app/job/page.js
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import Hero from "@/components/Hero";
+import Nav from "@/components/Nav";
+export default function Job() {
+  return (
+    <div>
+      <Nav />
+      <Hero />
+    </div>
+  );
+}
