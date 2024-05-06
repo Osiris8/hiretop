9fb77eee1821e76e07910798ea004d2207df9510
Osiris8 <miganosiris8@gmaaail.com>
2024-05-06 10:09:19 +0200

    add Nav component

--- app/page.js
+++ app/page.js
@@ -1,6 +1,12 @@
 import Image from "next/image";
 import Hero from "@/components/Hero";
+import Nav from "@/components/Nav";
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <div>
+      <Nav />
+      <Hero />
+    </div>
+  );
 }
