edc3dd6f3eb5ec09a6072f6fa325407a5126c943
Osiris8 <miganosiris8@gmaaail.com>
2024-05-07 22:28:08 +0200

    update Hero section

--- components/Hero.js
+++ components/Hero.js
@@ -9,8 +9,8 @@ function Hero() {
       <section className="bg-white">
         <div class="px-8 py-12 mx-auto md:px-12 lg:px-32 max-w-7xl">
           <div class="grid items-center grid-cols-1 gap-4 list-none lg:grid-cols-2 lg:gap-24 md:text-center sm:text-center lg:text-left">
-            <div>
-              <p class="font-medium md:text-6xl text-2xl text-slate-900 mt-8 tracking-tight text-balance">
+            <div className="">
+              <p class="font-medium lg:text-6xl md:text-6xl text-2xl text-slate-900 mt-8 tracking-tight text-balance">
                 Leading Linkeding Network Manager
               </p>
               <p class="mt-4 text-base font-medium text-gray-500">
@@ -18,18 +18,18 @@ function Hero() {
                 more control over their data and transactions, and the platform
                 is less susceptible to malicious attacks.
               </p>
-              <div class="">
+              <div class="mt-6 sm:text-center md:text-center lg:text-left">
                 <button
                   class="font-medium text-sm active:bg-fuchsia-50 active:text-black bg-indigo-50 focus-visible:outline-2 focus-visible:outline-fuchsia-50 focus-visible:outline-offset-2 focus:outline-none hover:text-indigo-700 justify-center px-6 py-2.5 rounded-lg text-indigo-600"
                   aria-label="Primary action"
                 >
-                  Primary button
+                  Talents
                 </button>
                 <button
-                  class="text-sm py-2  bg-indigo-500 focus-visible:outline-2 focus-visible:outline-fuchsia-50 focus-visible:outline-offset-2 focus:outline-none font-medium group hover:bg-indigo-50 hover:text-indigo-700 justify-center px-6 rounded-lg text-white"
+                  class="ml-4 text-sm py-2  bg-indigo-500 focus-visible:outline-2 focus-visible:outline-fuchsia-50 focus-visible:outline-offset-2 focus:outline-none font-medium group hover:bg-indigo-50 hover:text-indigo-700 justify-center px-6 rounded-lg text-white"
                   aria-label="Secondary action"
                 >
-                  Secondary button
+                  Entreprise
                 </button>
               </div>
             </div>
