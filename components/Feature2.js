7e1195713479905ab57a10ff6a9bf81279597b5f
Osiris8 <miganosiris8@gmaaail.com>
2024-05-11 22:40:25 +0200

    update content for Feature2 component

--- components/Feature2.js
+++ components/Feature2.js
@@ -8,28 +8,27 @@ export default function Feature2() {
         <div class="grid items-center grid-cols-1 gap-4 list-none lg:grid-cols-2 lg:gap-24">
           <div>
             <span class="text-xs font-bold tracking-wide text-gray-500 uppercase">
-              data
+              Entreprises
             </span>
             <p class="mt-8 text-4xl font-semibold tracking-tight text-gray-900 text-balance">
-              Empowering users with data control daily
+              Découvrez des talents exceptionnels pour votre entreprise.
             </p>
             <p class="mt-4 text-base font-medium text-gray-500">
-              Control and added security. With decentralization, users have more
-              control over their data and transactions, and the platform is less
-              susceptible to malicious attacks.
+              Notre plateforme vous offre un accès privilégié à une communauté
+              diversifiée de professionnels hautement qualifiés, prêts à relever
+              les défis de votre entreprise et à apporter une valeur ajoutée
+              inestimable à votre équipe. Avec une recherche intuitive et des
+              outils de filtrage avancés, trouvez rapidement les talents qui
+              correspondent parfaitement à vos besoins spécifiques et
+              préparez-vous à atteindre de nouveaux sommets de performance et
+              d'innovation.
             </p>
             <div class="flex flex-col items-center gap-2 mx-auto mt-8 md:flex-row">
               <button
-                class="inline-flex items-center justify-center w-full h-12 gap-3 px-5 py-3 font-medium text-white duration-200 bg-gray-900 md:w-auto rounded-xl hover:bg-gray-700 focus:ring-2 focus:ring-offset-2 focus:ring-black"
+                class="text-left inline-flex items-center justify-center w-full h-12 gap-3 px-5 py-3 font-medium text-white duration-200 bg-gray-900 md:w-auto rounded-xl hover:bg-gray-700 focus:ring-2 focus:ring-offset-2 focus:ring-black"
                 aria-label="Primary action"
               >
-                Primary button
-              </button>
-              <button
-                class="inline-flex items-center justify-center w-full h-12 gap-3 px-5 py-3 font-medium duration-200 bg-gray-100 md:w-auto rounded-xl hover:bg-gray-200 focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-                aria-label="Secondary action"
-              >
-                Secondary button
+                Démarrez
               </button>
             </div>
           </div>
