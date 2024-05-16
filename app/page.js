6bb1f4079086ea3d868236a91578eb273d62481f
Osiris8 <miganosiris8@gmaaail.com>
2024-05-16 17:22:44 +0200

    add edgestore modules

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 import Image from "next/image";
-import Hero from "@/components/Hero";
-import Nav from "@/components/Nav";
-import Trusted from "@/components/Trusted";
-import Feature1 from "@/components/Feature1";
-import Feature2 from "@/components/Feature2";
-import FAQSection from "@/components/Faq";
-import Cta from "@/components/Cta";
-import Footer from "@/components/Footer";
+import Hero from "../components/Hero";
+import Nav from "../components/Nav";
+import Trusted from "../components/Trusted";
+import Feature1 from "../components/Feature1";
+import Feature2 from "../components/Feature2";
+import FAQSection from "../components/Faq";
+import Cta from "../components/Cta";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
