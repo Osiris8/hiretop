50687c17a754e48b7e0219f3413c4c98d05cab07
Osiris8 <miganosiris8@gmaaail.com>
2024-05-19 19:48:23 +0200

    update Feature2 component

--- components/Feature2.js
+++ components/Feature2.js
@@ -1,20 +1,21 @@
 "use client";
 import Image from "next/image";
-import feature1 from "../public/images/feature/feature1.png";
 import company from "../public/images/feature/company.jpg";
+import { RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components";
+
 export default function Feature2() {
   return (
     <section id="companies">
-      <div class="px-8 py-12 mx-auto md:px-12 lg:px-32 max-w-7xl">
-        <div class="grid items-center grid-cols-1 gap-4 list-none lg:grid-cols-2 lg:gap-24">
-          <div>
-            <span class="text-xs font-bold tracking-wide text-gray-500 uppercase">
+      <div className="px-8 py-12 mx-auto md:px-12 lg:px-32 max-w-7xl">
+        <div className="grid items-center grid-cols-1 gap-4 list-none lg:grid-cols-2 lg:gap-24">
+          <div className="text-left">
+            <span className="text-xs font-bold tracking-wide text-gray-500 uppercase">
               Entreprises
             </span>
-            <p class="mt-8 text-4xl font-semibold tracking-tight text-gray-900 text-balance">
+            <p className="mt-8 text-4xl font-semibold tracking-tight text-gray-900 text-balance">
               Découvrez des talents exceptionnels pour votre entreprise.
             </p>
-            <p class="mt-4 text-base font-medium text-gray-500">
+            <p className="mt-4 text-base font-medium text-gray-500">
               Notre plateforme vous offre un accès privilégié à une communauté
               diversifiée de professionnels hautement qualifiés, prêts à relever
               les défis de votre entreprise et à apporter une valeur ajoutée
@@ -24,20 +25,20 @@ export default function Feature2() {
               préparez-vous à atteindre de nouveaux sommets de performance et
               d&apos;innovation.
             </p>
-            <div class="flex flex-col items-center gap-2 mx-auto mt-8 md:flex-row">
+            <div className="flex mt-8">
               <button
-                class="text-left inline-flex items-center justify-center w-full h-12 gap-3 px-5 py-3 font-medium text-white duration-200 bg-gray-900 md:w-auto rounded-xl hover:bg-gray-700 focus:ring-2 focus:ring-offset-2 focus:ring-black"
+                className="inline-flex items-center justify-center w-full h-12 gap-3 px-5 py-3 font-medium text-white duration-200 bg-gray-900 md:w-auto rounded-xl hover:bg-gray-700 focus:ring-2 focus:ring-offset-2 focus:ring-black"
                 aria-label="Primary action"
               >
-                Démarrez
+                <RegisterLink>Demarrez</RegisterLink>
               </button>
             </div>
           </div>
-          <div class="p-2 border bg-gray-50 rounded-3xl lg:order-first">
-            <div class="h-full overflow-hidden bg-white border shadow-lg rounded-3xl">
+          <div className="p-2 border bg-gray-50 rounded-3xl">
+            <div className="h-full overflow-hidden bg-white border shadow-lg rounded-3xl">
               <Image
                 alt="Lexingtøn thumbnail"
-                class="relative w-full rounded-2xl drop-shadow-2xl"
+                className="relative w-full rounded-2xl drop-shadow-2xl"
                 src={company}
               />
             </div>
