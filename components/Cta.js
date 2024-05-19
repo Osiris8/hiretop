bc1f160af058b11a9fb932e87464be761978c5e8
Osiris8 <miganosiris8@gmaaail.com>
2024-05-19 19:51:00 +0200

    update CTA component

--- components/Cta.js
+++ components/Cta.js
@@ -1,5 +1,7 @@
 "use client";
 
+import { RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components";
+
 export default function Cta() {
   return (
     <section>
@@ -20,13 +22,13 @@ export default function Cta() {
                 class="inline-flex items-center justify-center w-full h-12 gap-3 px-5 py-3 font-medium text-white duration-200 bg-gray-900 md:w-auto rounded-xl hover:bg-gray-700 focus:ring-2 focus:ring-offset-2 focus:ring-black"
                 aria-label="Primary action"
               >
-                Je suis un talent
+                <RegisterLink>Je suis un talent</RegisterLink>
               </button>
               <button
                 class="inline-flex items-center justify-center w-full h-12 gap-3 px-5 py-3 font-medium duration-200 bg-gray-100 md:w-auto rounded-xl hover:bg-gray-200 focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                 aria-label="Secondary action"
               >
-                Je cherche des Talents
+                <RegisterLink>Je cherche des Talents</RegisterLink>
               </button>
             </div>
           </div>
