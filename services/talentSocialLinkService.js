a0fa784f9fec593ab13263ff1e404ddcbd7d5237
Osiris8 <miganosiris8@gmaaail.com>
2024-05-20 22:38:55 +0200

    delete services folder

--- services/talentSocialLinkService.js
+++
@@ -1,8 +0,0 @@
-// services/talentSocialLinkService.js
-export const updateTalentSocialLink = async (userId, socialLinks) => {
-  const response = await fetch(`/api/talent-social-link/${userId}`, {
-    method: "PATCH",
-    body: JSON.stringify(socialLinks),
-  });
-  return response;
-};
