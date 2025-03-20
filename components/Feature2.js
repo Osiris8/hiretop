34b1ad2249609958ac9449fdb147d149b18b9f78
Osiris8 <miganosiris8@gmaaail.com>
2025-03-20 15:49:39 +0100

    Update Feature2

--- components/Feature2.js
+++ components/Feature2.js
@@ -10,27 +10,25 @@ export default function Feature2() {
         <div className="grid items-center grid-cols-1 gap-4 list-none lg:grid-cols-2 lg:gap-24">
           <div className="text-left">
             <span className="text-xs font-bold tracking-wide text-gray-500 uppercase">
-              Entreprises
+              Company
             </span>
             <p className="mt-8 text-4xl font-semibold tracking-tight text-gray-900 text-balance">
-              Découvrez des talents exceptionnels pour votre entreprise.
+              Discover exceptional talents for your company.
             </p>
             <p className="mt-4 text-base font-medium text-gray-500">
-              Notre plateforme vous offre un accès privilégié à une communauté
-              diversifiée de professionnels hautement qualifiés, prêts à relever
-              les défis de votre entreprise et à apporter une valeur ajoutée
-              inestimable à votre équipe. Avec une recherche intuitive et des
-              outils de filtrage avancés, trouvez rapidement les talents qui
-              correspondent parfaitement à vos besoins spécifiques et
-              préparez-vous à atteindre de nouveaux sommets de performance et
-              d&apos;innovation.
+              Our platform gives you privileged access to a diverse community of
+              highly skilled professionals, ready to take on your company&#39;s
+              challenges and bring invaluable value to your team. With intuitive
+              search and advanced filtering tools, quickly find the talents that
+              perfectly match your specific needs and get ready to reach new
+              heights of performance and innovation.
             </p>
             <div className="flex mt-8">
               <button
                 className="inline-flex items-center justify-center w-full h-12 gap-3 px-5 py-3 font-medium text-white duration-200 bg-gray-900 md:w-auto rounded-xl hover:bg-gray-700 focus:ring-2 focus:ring-offset-2 focus:ring-black"
                 aria-label="Primary action"
               >
-                <RegisterLink>Demarrez</RegisterLink>
+                <RegisterLink>Get started</RegisterLink>
               </button>
             </div>
           </div>
